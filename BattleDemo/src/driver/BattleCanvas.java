8bc43df66c9570e2408d363d3dba9e9af5214800
youngJeezy858 <kfrank@mix.wvu.edu>
2014-03-06 11:23:23 -0500

    added a health bar and stamina bar

--- BattleDemo/src/driver/BattleCanvas.java
+++ BattleDemo/src/driver/BattleCanvas.java
@@ -21,15 +21,19 @@ public class BattleCanvas extends ECanvas {
 
 	private EAnimation enemy_currentAction;
 	private EAnimation enemy_idle;
+	private int enemy_health;
 	private int enemy_x;
 	private int enemy_y;
 	
 	private int enemy_state;
+	public static int ENEMY_READY = 0;
+	public static int ENEMY_BUSY = 1;
 	
 	private EAnimation player_currentAction;
 	private EAnimation player_idle;
 	private EAnimation player_walking;
 	private EAnimation player_kick;
+	private int player_stamina;
 	private int player_x;
 	private int player_y;
 	
@@ -41,6 +45,7 @@ public class BattleCanvas extends ECanvas {
 	private int frame_hit;
 	private int[] frame_hit_x;
 	private int[] frame_hit_y;
+
 	
 	private BattleCanvas(){
 		super(1);
@@ -72,10 +77,9 @@ public class BattleCanvas extends ECanvas {
 		player_kick = new EAnimation(tempC, new int[]{300,200,200,100});
 		
 		temp = EAnimation.loadImage("/images/enemy_idle.png");
-		ss = new SpriteSheet(temp, temp.getWidth()/2, temp.getHeight());
-		Image tempD[] = {ss.getSprite(0, 0), ss.getSprite(1, 0)};
+		ss = new SpriteSheet(temp, temp.getWidth()/3, temp.getHeight());
+		Image tempD[] = {ss.getSprite(0, 0), ss.getSprite(1, 0), ss.getSprite(2, 0)};
 		enemy_idle = new EAnimation(tempD, 100);
-		
 		
 		player_currentAction = player_idle;
 		player_x = 400;
@@ -85,7 +89,7 @@ public class BattleCanvas extends ECanvas {
 		enemy_currentAction = enemy_idle;
 		enemy_x = 800;
 		enemy_y = 400;
-		enemy_state = 0;
+		enemy_state = ENEMY_READY;
 		
 		background_x = 0;
 		background_y = 0;
@@ -93,6 +97,8 @@ public class BattleCanvas extends ECanvas {
 		frame_hit = -1;
 		frame_hit_x = new int[] {};
 		frame_hit_y = new int[] {};
+		enemy_health = 100;
+		player_stamina = 100;
 	}
 
 	@Override
@@ -100,6 +106,8 @@ public class BattleCanvas extends ECanvas {
 		background.draw(background_x, background_y);
 		enemy_currentAction.getCurrentFrame().draw(enemy_x, enemy_y);
 		player_currentAction.getCurrentFrame().draw(player_x, player_y);
+		g.drawString(new Integer(player_stamina).toString(), 50f, 50f);
+		g.drawString(new Integer(enemy_health).toString(), 800f, 50f);
 	}
 
 	@Override
@@ -110,42 +118,47 @@ public class BattleCanvas extends ECanvas {
 			        
    		if (frame_stop != -1 && player_state == PLAYER_BUSY){
    			int currentFrame = player_currentAction.getFrame();
-			if (player_currentAction.getFrame() == frame_hit){
+			if (player_currentAction.getFrame() == frame_hit && enemy_currentAction.getFrame() != 2){
 				//for all enemies
 				for (int i = 0; i < frame_hit_x.length; i++){
 					int x = frame_hit_x[i] + player_x - enemy_x;
 					int y = frame_hit_y[i] + player_y - enemy_y;
-					System.out.println(x + " " + y);
 					Image frame = enemy_currentAction.getCurrentFrame();
 					int xBound = frame.getWidth();
 					int yBound = frame.getHeight();
 					if (x < 0 || x > xBound || y < 0 || y > yBound)
 						continue;
 					Color c = frame.getColor(x, y);
-					if (c.a != 0){
-						enemy_currentAction.setCurrentFrame(Math.abs(enemy_currentAction.getFrame() - 1));
+					if (c.a != 0 && enemy_state == ENEMY_READY){
+						enemy_state = ENEMY_BUSY;
+						enemy_health = enemy_health - 10;
+						if (enemy_health <= 0)
+							enemy_currentAction.setCurrentFrame(2);
+						else
+							enemy_currentAction.setCurrentFrame(Math.abs(enemy_currentAction.getFrame() - 1));
 						break;
 					}
 				}		
-				System.out.println("kick end");
 			}
 			else if (currentFrame == frame_stop){
 				player_state = PLAYER_READY;
+				enemy_state = ENEMY_READY;
 			}
 			return;
 		}
 		else
 			player_state = PLAYER_READY;
 		
-		if (input.isKeyPressed(Input.KEY_SPACE)){
+		if (input.isKeyPressed(Input.KEY_SPACE) && player_stamina >= 10){
+			player_stamina = player_stamina - 10;
 			player_state = PLAYER_BUSY;
 			player_currentAction = player_kick;
 			player_currentAction.setCurrentFrame(0);
-			System.out.println("kick begin");
 			frame_hit = 1;
 			frame_stop = 3;
 			frame_hit_x = new int[] {116, 132, 162, 192, 217};
 			frame_hit_y = new int[] {94, 74, 52, 27, 25};
+			return;
 		}
 		
 		else if (input.isKeyDown(Input.KEY_W) || input.isKeyDown(Input.KEY_A)
@@ -185,7 +198,9 @@ public class BattleCanvas extends ECanvas {
 			player_currentAction = player_idle;
 			player_walking.setCurrentFrame(0);
 		}
-
+		
+		if (player_stamina < 100)
+			player_stamina++;
 	}
 
 }
